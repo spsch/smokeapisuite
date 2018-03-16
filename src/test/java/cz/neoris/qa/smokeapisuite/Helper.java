1755a92a1d99ec5a073a0c126cef8122c59669a8
jan.svehlak@neoris.com <jan.svehlak@neoris.com>
2018-03-16 12:07:45 +0100

    Initial commit

---
+++ src/test/java/cz/neoris/qa/smokeapisuite/Helper.java
@@ -0,0 +1,10 @@
+package cz.neoris.qa.smokeapisuite;
+
+public interface Helper {
+    final String IBMCLIENTID = "721e5c7b-73b8-40e1-8cb2-31c7dbdbd1be";
+    final String MXUSER = "payments.customer01@gmail.com";
+    final String PWD = "TestCycleV2CemexGo";
+    final String APPCODE = "DCMWEBPORTAL";
+    final String BASEURL = "https://api.us2.apiconnect.ibmcloud.com/cnx-gbl-org-quality/quality";
+
+}
